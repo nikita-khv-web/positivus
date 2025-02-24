4d5fb4df146d7314866e923830936caf3ee61217
Nikita Khveshchenko <nikita.khv.dev@yandex.ru>
2025-02-24 23:50:12 +0300

    feat: add section header and mobile overlay

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 import "../scss/style.scss";
+
+const buttonBurger = document.querySelector(".button-burger");
+const mobileOverlay = document.querySelector("#mobileOverlay");
+
+buttonBurger.addEventListener("click", () => {
+	mobileOverlay.showModal();
+});
